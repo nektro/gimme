e3754e141a1f4aafcde0d7f4816057ea9f15ec09
Meghan Denny <hello@nektro.net>
2023-03-01 21:38:09 -0800

    add the PanicAllocator

--- src/lib.zig
+++ src/lib.zig
@@ -1,2 +1,3 @@
 pub const FailingAllocator = @import("./FailingAllocator.zig");
 pub const UnreachableAllocator = @import("./UnreachableAllocator.zig");
+pub const PanicAllocator = @import("./PanicAllocator.zig");
