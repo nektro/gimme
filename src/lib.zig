a9d66a00d3a55a1635456a1afe62071df2b6a302
Meghan Denny <hello@nektro.net>
2023-03-01 21:39:21 -0800

    add the LivenessAllocator

--- src/lib.zig
+++ src/lib.zig
@@ -2,3 +2,4 @@ pub const FailingAllocator = @import("./FailingAllocator.zig");
 pub const UnreachableAllocator = @import("./UnreachableAllocator.zig");
 pub const PanicAllocator = @import("./PanicAllocator.zig");
 pub const CountingAllocator = @import("./CountingAllocator.zig");
+pub const LivenessAllocator = @import("./LivenessAllocator.zig");
--- src/main.zig
+++ src/main.zig
@@ -3,4 +3,5 @@ const gimme = @import("gimme");
 test {
     _ = gimme.FailingAllocator;
     _ = gimme.CountingAllocator;
+    _ = gimme.LivenessAllocator;
 }
