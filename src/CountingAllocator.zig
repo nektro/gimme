d9ad4f3abdbf8160c434aacef394a15c7f4830fc
Meghan Denny <hello@nektro.net>
2023-05-02 23:01:03 -0700

    CountingAllocator: add count_active_strict

--- src/CountingAllocator.zig
+++ src/CountingAllocator.zig
@@ -4,12 +4,14 @@ const extras = @import("extras");
 
 child_allocator: std.mem.Allocator,
 count_active: u64,
+count_active_strict: u64,
 count_total: u64,
 
 pub fn init(child_allocator: std.mem.Allocator) CountingAllocator {
     return .{
         .child_allocator = child_allocator,
         .count_active = 0,
+        .count_active_strict = 0,
         .count_total = 0,
     };
 }
@@ -35,17 +37,20 @@ fn alloc(ctx: *anyopaque, len: usize, ptr_align: u8, ret_addr: usize) ?[*]u8 {
 
 fn resize(ctx: *anyopaque, buf: []u8, buf_align: u8, new_len: usize, ret_addr: usize) bool {
     var self = extras.ptrCast(CountingAllocator, ctx);
+    const old_len = buf.len;
     const stable = self.child_allocator.rawResize(buf, buf_align, new_len, ret_addr);
+    self.count_active += new_len;
+    self.count_active -= old_len;
     if (!stable) {
-        self.count_active -= buf.len;
-        self.count_active += new_len;
-        self.count_total += new_len;
+        self.count_active_strict += new_len;
+        self.count_active_strict -= old_len;
     }
+    self.count_total += new_len;
     return stable;
 }
 
 fn free(ctx: *anyopaque, buf: []u8, buf_align: u8, ret_addr: usize) void {
     var self = extras.ptrCast(CountingAllocator, ctx);
-    defer self.count_active -= buf.len;
+    self.count_active -= buf.len;
     return self.child_allocator.rawFree(buf, buf_align, ret_addr);
 }
