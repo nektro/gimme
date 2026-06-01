3956e1117a7fc64b82734c3e3b46072411b4e191
Meghan Denny <hello@nektro.net>
2026-06-01 16:05:35 -0700

    update to zig 0.15.2

--- build.zig
+++ build.zig
@@ -3,13 +3,15 @@ const deps = @import("./deps.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
+    const mode = b.option(std.builtin.OptimizeMode, "mode", "") orelse .Debug;
     const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
 
     const exe_tests = b.addTest(.{
-        .root_source_file = b.path("test.zig"),
-        .target = target,
-        .optimize = mode,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("test.zig"),
+            .target = target,
+            .optimize = mode,
+        }),
     });
     deps.addAllTo(exe_tests);
     exe_tests.use_llvm = !disable_llvm;
